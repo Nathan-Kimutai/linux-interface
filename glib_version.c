c8b2d2003da5afbb4c974c7313a7a814171165ce
Nathan <nathanpykimutai@gmail.com>
2020-09-04 16:16:10 +0300

    added the header files for the project

--- glib_version.c
+++ glib_version.c
@@ -1,8 +1,15 @@
 #include <gnu/libc-version.h>
 
-/* This program is used to print the gnu libc version number */
+/* This program is used to print the gnu libc version number 
+ * You can also use the confstr to get the configuration of _CS_GNU_LIBC_VERSION
+ * The confstr() returns string such as glibc 2.12.
+ * */
 
 const char *gnu_get_libc_version(void);
+
+void confstr_get_version() {
+  prinf("The gnu libc version is");
+}
 
 int main()
 {
